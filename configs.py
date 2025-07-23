896e9b867b5468a453e5e073f7ae8c83f9e76ba5
Xjdjsjs <a23568831@gmail.com>
2025-07-23 09:23:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6747520930"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002087398411")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002529877348"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
